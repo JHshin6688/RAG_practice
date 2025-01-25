b3ceb9a4143cb8f2967b807b06f74c97af0c329b
unknown <robotics0608@gmail.com>
2025-01-25 21:50:03 +0900

    added link extraction functionality (not finished)

--- streamlit_rag.py
+++ streamlit_rag.py
@@ -1,6 +1,18 @@
-import streamlit as st
+from openai import OpenAI
 
-st.title("🔎 LangChain - Chat with automatic Search")
+import streamlit as st
+import re
+
+
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="langchain_search_api_key_openai", type="password"
+    )
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
+
+
+st.title("🔎 LangChain - Auto Search")
 
 # Initialize session state for storing messages
 if "messages" not in st.session_state:
@@ -8,20 +20,34 @@ if "messages" not in st.session_state:
         {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
     ]
 
+
 # Display chat messages
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-# Create input fields for question and related link
-question = st.text_input("What is your question?")
-web_link = st.text_input("Provide a related link")
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-# Check if both inputs are provided
-if question and web_link:
-    # Append the user's question to messages
-    st.session_state.messages.append({"role": "user", "content": question})
-    st.chat_message("user").write(question)
+    client = OpenAI(api_key=openai_api_key)
+    st.chat_message("user").write(prompt)
 
-    # Append the user's link to messages
-    st.session_state.messages.append({"role": "link", "content": web_link})
-    st.chat_message("link").write(web_link)
+    #augment the prompt
+    prompt += "Please provide relevant links that can be helpful for me"
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
+    st.chat_message("assistant").write(response)
+
+    urls = re.findall(r'https?://[^\s\)]+', msg)
+
+    st.write(urls)
+    print(urls)
+
+    #st.markdown("![Alt Text](https://www.python.org/)")
+    #st.image("https://www.python.org/")
