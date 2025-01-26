88ea3ce65d6670948dea82b22aa31727583c851c
Jeeho <robotics0608@gmail.com>
2025-01-26 23:38:51 +0900

    NFL_RAG first commit

---
+++ RAG.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
+USER_AGENT = os.getenv("USER_AGENT")
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain import hub
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="langchain_search_api_key_openai", type="password"
+    )
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
+
+
+st.title("🔎 LangChain - NFL Search")
+
+
+# Initialize session state for storing messages
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
+    ]
+
+
+# Display chat messages
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+# Construct the vector database
+web_loader = WebBaseLoader([
+    "https://www.nfl.com/news/all-news",
+    "https://www.nfl.com/news/sza-to-join-kendrick-lamar-as-guest-during-super-bowl-halftime-performance"
+    ]
+)
+
+data = web_loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500, 
+    chunk_overlap = 0
+)
+
+all_splits = text_splitter.split_documents(data)
+
+vectorstore = Chroma.from_documents(
+    documents=all_splits, 
+    embedding=OpenAIEmbeddings(),
+    collection_name= "handbook",
+    persist_directory= "rag_db"
+    )
+
+template = hub.pull("rlm/rag-prompt")
+
+
+# Get question by the user
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    st.chat_message("user").write(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Create RAG agent
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    model = ChatOpenAI(model_name="gpt-4", temperature=0)
+
+    qa_chain = RetrievalQA.from_llm(
+        llm=model, 
+        retriever=vectorstore.as_retriever(),
+        prompt = template
+    )
+
+    result = qa_chain.invoke({"query": prompt})
+
+    msg = result["result"]
+
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
--- simple_rag.py
+++ simple_rag.py
@@ -18,9 +18,8 @@ from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 
 web_loader = WebBaseLoader([
-    #"https://python.langchain.com/docs/get_started/introduction",   # LangChain Introduction
-    #"https://python.langchain.com/docs/modules/data_connection/" # LangChain Retrieval
-    "https://www.nba.com/news/power-rankings-2024-25-week-13"
+    "https://www.nfl.com/news/all-news",
+    "https://www.nfl.com/news/sza-to-join-kendrick-lamar-as-guest-during-super-bowl-halftime-performance"
     ]
 )
 
@@ -50,45 +49,6 @@ qa_chain = RetrievalQA.from_llm(
     prompt = prompt
 )
 
-question1 = "Which two teams have the highest power ranking in NBA?"
+question1 = "What is the relationship between SZA and Kendrick Lamar?"
 result1 = qa_chain.invoke({"query": question1})
 print(result1["result"])
-
-#question2 = "What is retrieval in LangChain?"
-#result2 = qa_chain.invoke({"query" : question2})
-#print(result2["result"])
-
-
-'''
-    with st.spinner("Processing.."):
-        web_loader = WebBaseLoader(web_path= [web_link])
-
-        data = web_loader.load()
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 500,
-            chunk_overlap = 0
-        )
-
-        all_splits = text_splitter.split_documents(data)
-
-        vectorstore = Chroma.from_documents(
-            documents=all_splits, 
-            embedding=OpenAIEmbeddings(),
-            collection_name= "handbook",
-            persist_directory= "rag_db"
-            )
-
-        template = hub.pull("rlm/rag-prompt")
-
-        model = ChatOpenAI(model_name="gpt-4", temperature=0)
-
-        qa_chain = RetrievalQA.from_llm(
-            llm=model, 
-            retriever=vectorstore.as_retriever(),
-            prompt = template
-        )
-
-        result = qa_chain.invoke({"query": web_link})
-        st.session_state.messages.append({"role":"assistant", "content" : result["result"]})
-        st.chat_message("assistant").write(result["result"])'''
